0b9ee36f8bf80821ff4437269b5cc07e8ed089d6
nglmk2002 <80315785+nglmk2002@users.noreply.github.com>
2021-04-22 18:12:52 +0900

    2021-04-22

--- object_detection_camera.py
+++ object_detection_camera.py
@@ -41,5 +41,72 @@ if not os.path.exists(PATH_TO_LABELS):
 
 ## 모델 로딩!
 from object_detection.utils import label_map_util
-from object_detection.utils import config
+from object_detection.utils import config_util
 from object_detection.utils import visualization_utils as viz_utils
+from object_detection.builders import model_builder
+
+#GPU의 dynamic Memory Allocation 을 활성화 시키는 코드
+
+gpus = tf.config.experimental.list_logical_devices('GPU')
+for gpu in gpus :
+    tf.config.experimental.set_memory_growth(gpu,True)
+
+#config 로드하고 모델 빌드
+configs = config_util.get_configs_from_pipeline_file(PATH_TO_CFG)
+model_config = configs['model']
+detection_model = model_builder.build(model_config=model_config,is_training=False)
+
+#Restore Checkpoint
+
+ckpt = tf.compat.v2.train.Checkpoint(model = detection_model)
+ckpt.restore(os.path.join(PATH_TO_CKPT,'ckpt-0')).expect_partial()
+
+@tf.function
+def detect_fn(image) :
+    """Detect Object in Image."""
+    image,shapes = detection_model.preprocess(image)
+    prediction_dict = detection_model.predict(image,shapes)
+    detections=detection_model.postprocess(prediction_dict,shapes)
+    return detections,prediction_dict,tf.reshape(shapes,[-1])
+
+#레이블 맵 데이터로딩(load label map data)
+category_index = label_map_util.create_categories_from_labelmap(PATH_TO_LABELS,use_display_name=True)
+print(category_index)
+
+import cv2
+import numpy as np
+import pandas as pd
+
+cap = cv2.VideoCapture(0)
+while True :
+    ret, image_np = cap.read()
+    #(1,x,x,3) 이렇게 만든다.
+    image_np_expended = np.expand_dims(image_np,axis=0)
+
+    input_tensor = tf.convert_to_tensor(np.expand_dims(image_np,0),dtype=tf.float32)
+    detections,prediction_dict,shapes = detect_fn(input_tensor)
+    # print(detections)
+    #mscoco_label_map.pbtxt 파일을 보면,id가 1부터 시작하니까
+    #offset 을 1로 만들어준다.
+    label_id_offset = 1
+    image_np_with_detections = image_np.copy()
+    viz_utils.visualize_boxes_and_labels_on_image_array(
+        image_np_with_detections,detections['detection_boxes'][0].numpy(),
+        (detections['detection_classes'][0].numpy()+label_id_offset).astype(int),
+        detections['detection_scores'][0].numpy(),
+        category_index,
+        use_normalized_coordinates=True,
+        max_boxes_to_draw=200,
+        min_score_thresh = 0.6
+    )
+    #cv2.imshow("object detection",image_np_with_detections)
+    #관제시스템의 경우 디스플레이 크면 거기에 맞게 조절해서 보여주도록 하는 코드
+    cv2.imshow("object detection",cv2.resize(image_np_with_detections,(800,600)))
+    
+    if cv2.waitKey(25) & 0xFF == 27 :
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+    
